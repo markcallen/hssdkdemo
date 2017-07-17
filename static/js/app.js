605af3158dd8796f68a3ff9a97fa5df01bf0dda0
Mark C Allen <mark.allen@hootsuite.com>
2017-07-17 14:52:17 -0700

    Fixed sendtoapp

--- static/js/app.js
+++ static/js/app.js
@@ -96,25 +96,6 @@ $(document).ready(function() {
         }));
     });
 
-    hsp.bind('sendtoapp', function(message){
-        var userid = message.post.user.userid;
-        var username = message.post.user.username;
-        var avatar;
-        if (message.post.network == 'twitter') {
-            avatar = 'http://avatars.io/twitter/' + username;
-        } else {
-            avatar = 'http://avatars.io/facebook/' + userid;
-        }
-
-
-        $('.hs_topBar').after(messageTemplate({
-            input: message.post.content.body,
-            username: username,
-            avatar: avatar,
-            messageId: parseInt(1000000*Math.random(), 10) //generate random message ID for demonstration purposes
-        }));
-    });
-
     hsp.bind('sendprofiletoapp', function(profile){
 
         if (profile.network == 'TWITTER') {
